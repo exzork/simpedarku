b04615da84fd014524e081b0a28eae91f24dbf83
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-02-24 02:18:31 +0700

    fix typo

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin'=> \App\Http\Middleware\Admin::class,
-        'stakeholder'=> \App\Http\Middleware\Stakeholder::class,
+        'stakeholder'=> \App\Http\Middleware\StakeHolder::class,
     ];
 }
