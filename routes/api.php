a55ff7b8cd992b7dd44f971aad26e85c34c93a5b
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-02-26 11:42:32 +0700

    add endpoint for api logout

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->name('api.')->group(function(){
     Route::apiResource('reports',\App\Http\Controllers\Api\V1\ReportController::class);
     Route::get('/user/profile',[\App\Http\Controllers\Api\V1\UserController::class,'profile'])->name('user.profile.show');
     Route::put('/user/profile',[\App\Http\Controllers\Api\V1\UserController::class,'updateProfile'])->name('user.profile.update');
-    Route::post('/logout',[\App\Http\Controllers\Api\V1\UserController::class,'logout'])->name('user.logout');
+    Route::post('/user/logout',[\App\Http\Controllers\Api\V1\UserController::class,'logout'])->name('user.logout');
     Route::apiResource('users',\App\Http\Controllers\Api\V1\UserController::class);
 
 });
