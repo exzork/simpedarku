82bf1da822d0e682550ae30d0e7e301601e167ad
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-02-26 20:40:13 +0700

    fix api response when registering with api

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use App\Traits\ResponseTrait;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -15,6 +16,7 @@ use Illuminate\Validation\Rules;
 
 class RegisteredUserController extends Controller
 {
+    use ResponseTrait;
     /**
      * Display the registration view.
      *
@@ -57,6 +59,6 @@ class RegisteredUserController extends Controller
 
         $user =  UserResource::make(\auth()->user())->api_token($api_token);
 
-        return $request->wantsJson() ? $this->success(['user'=>$user]): redirect()->intended(RouteServiceProvider::HOME);
+        return $request->wantsJson() ? $this->success(['user'=>$user]): redirect(RouteServiceProvider::HOME);
     }
 }
