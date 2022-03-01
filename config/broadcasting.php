5b335ee04ec7dbee5ba5693df0a83dafbe5b044c
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-03-01 15:36:00 +0700

    add soketi(pusher compatible) configuration

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,8 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'host' => env('PUSHER_HOST'),
+                'port' => env('PUSHER_PORT'),
+                'scheme' => env('PUSHER_SCHEME'),
+                'encrypted' => true,
+                'useTLS' => env('PUSHER_SCHEME') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
