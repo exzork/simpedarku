ebefc815319e3bb2152df7f1834ea58fa85a5355
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-03-02 11:10:02 +0700

    fix renaming emergency_phone to emergency_contact

--- resources/views/admin/reports/show.blade.php
+++ resources/views/admin/reports/show.blade.php
@@ -119,7 +119,7 @@
                                                     {{ __('Nomor HP Kontak Darurat') }}
                                                 </dt>
                                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                                    {{ $report->user->emergency_phone }}
+                                                    {{ $report->user->emergency_contact }}
                                                 </dd>
                                             </div>
                                             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
